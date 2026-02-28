22746b156aabf3c46a54070f7dce6870206a5791
joeyzou <495043143@qq.com>
2026-02-28 12:27:19 +0800

    test: add test dependencies to feature:stats module

--- feature/stats/build.gradle.kts
+++ feature/stats/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     ksp(libs.hilt.compiler)
     
     testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
